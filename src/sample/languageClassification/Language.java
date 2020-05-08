5aa59d78e28809883535c363e358d2665f231ac8
musta55 <48786719+musta55@users.noreply.github.com>
2020-05-08 23:41:08 +0600

    BiLingual3

--- src/sample/languageClassification/Language.java
+++ src/sample/languageClassification/Language.java
@@ -1,12 +1,5 @@
 package sample.languageClassification;
 
-
-
-
-
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -84,161 +77,10 @@ public class Language {
 
 
         abi.setOnAction(e -> {
-            try {
-                primaryStage.setTitle("Text Input");
-
-                TextArea textFields = new TextArea();
-
-                Button button = new Button("Enter");
-                button.setTextFill(Color.WHITE);
-                setStyle(button);
-                button.setTranslateX(620);
-                button.setTranslateY(350);
-                button.setPrefSize(150,70);
-
-                try {
-                    Text headning = new Text("TEXT INPUT");
-                    headning.setFont(Font.font(Font.getFontNames().get(12), FontPosture.REGULAR, 11));
-                    headning.setFill(Color.DARKBLUE);
-                    headning.setScaleX(6);
-                    headning.setScaleY(6);
-                    headning.setTranslateX(650);
-                    headning.setTranslateY(90);
-                    Image backgrounds = new Image(getClass().getClassLoader().getResource("emotion(16-9).png").toString(), true);
-                    Canvas canvas = new Canvas(1600,900);
-                    GraphicsContext gc = canvas.getGraphicsContext2D();
-                    //gc.drawImage(backgrounds,0,0);
-
-                    textFields.setLayoutX(150);
-                    textFields.setLayoutY(180);
-                    textFields.setPrefRowCount(5);
-                    textFields.setPrefColumnCount(6);
-                    textFields.setWrapText(true);
-                    textFields.setMinSize(1125,150);
-                    Pane roots = new Pane();
-                    BackgroundImage bi = new BackgroundImage(backgrounds,
-                            BackgroundRepeat.NO_REPEAT,
-                            BackgroundRepeat.NO_REPEAT,
-                            BackgroundPosition.DEFAULT,
-                            BackgroundSize.DEFAULT);
-                    Background bg = new Background(bi);
-                    roots.setBackground(bg);
-                    roots.getChildren().addAll(canvas,textFields,button,headning);
-                    Scene scene = new Scene(roots,1400,750);
-                    primaryStage.setScene(scene);
-                    //primaryStage.setFullScreen(true);
-                    primaryStage.show();
-                }catch (Exception ex)
-                {
-                    System.out.println("Picture url Problem");
-                }
-
-                button.setOnAction(action -> {
-                    Operations operations = new Operations();
-
-                    OperationsBangla operationsBangla = new OperationsBangla();
-
-                    operationsBangla.splitInputBangla();
-                    String inputStringBan = "";
-                    ArrayList<String> inListBan = new ArrayList<String>();//to store the input words
-                    String[] inArray = new String[10000];
-                    String[] inArray2 = new String[10000];
-
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-                    Dictionary dictionary = null;
-                    try {
-                        dictionary = new Dictionary("isrc/spl1/Translaton.txt");
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-
-
-                    //     while ((inputStringBan = in.readLine()) != null) {
-                    //     while (nameInput.getText() != null) {
-
-                    // operations.splitInput(inputStringBan);
-                    //     if (inputStringBan.contains("###")) break;
-                    //     if( nameInput.getText().contains("###") )break;
-                    inArray = textFields.getText().split("[ ,/;>.*'|\"(){+></@$%^&_=}]", 0);
-
-                    for (int j = 0; j < inArray.length; j++) {
-                        //    System.out.println("askdas");
-                        inArray2[j] = operationsBangla.searchBan(inArray[j]);
-                        //    System.out.print("Bangla Language is ###################"+inArray2[j] + " ");
-                    }
-                    String inp = "";
-                    for (int j = 0; j < inArray.length; j++) {
-
-                        //  System.out.print(inArray[j]+" ");
-                        //   System.out.print(dictionary.search("\n\n\nDictionary search kore PAIiiiiiiiiiiiii"+inArray[j])+" ");
-                        inArray2[j] = dictionary.search(inArray[j]);
-                        //  inListBan.add(inArray2[j]);
-
-                        //   System.out.print(inArray2[4]+" ");
-
-                        inp = inp + inArray2[j] + " ";
-
-
-
-
-
-                    }
-
-
-                    //        System.out.println("\n\n\nTotal input in Bilingual is :"+inp);
-                    try {
-                        operations.splitInput(inp);
-                    } catch (FileNotFoundException ex) {
-                        ex.printStackTrace();
-                    }
-
-
-                    // inputStringBan = in.readLine();
-                    //  inputStringBan= "আমি সত্য ভালবাসা";
-
-
-                    try {
-                        operations.removeWord();
-                    } catch (FileNotFoundException ex) {
-                        ex.printStackTrace();
-                    }
-                    operations.search();
-
-
-                    EmotionCalculation emCal = new EmotionCalculation();
-
-                    try {
-                        emCal.searchEmotion();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        emCal.emotionCalc(primaryStage);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        emCal.VisualOutput(primaryStage);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-
-
-
-                    //     System.out.println("\n"+textFields.getText());
-                });
-//
-//              //  VBox vbox = new VBox(textField, button);
-//Pane roots=new Pane();
-//                Scene scene = new Scene(roots, 800, 100);
-//                primaryStage.setScene(scene);
-//                primaryStage.show();
-
-            } catch (Exception excep) {
-                excep.printStackTrace();
-            }
-
+          biLingual bl =new biLingual();
+          {
+              bl.biLanguage(primaryStage);
+          }
         });
 
 
@@ -313,12 +155,6 @@ public class Language {
                     }
 
 
-                    //     while ((inputStringBan = in.readLine()) != null) {
-                    //     while (nameInput.getText() != null) {
-
-                    // operations.splitInput(inputStringBan);
-                    //     if (inputStringBan.contains("###")) break;
-                    //     if( nameInput.getText().contains("###") )break;
                     inArray = textFields.getText().split("[ ,/;>.*'|\"(){+></@$%^&_=}]", 0);
 
                     for (int j = 0; j < inArray.length; j++) {
